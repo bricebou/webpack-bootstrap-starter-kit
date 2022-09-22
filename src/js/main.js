6182e0e9ed1513ad974eae19dcac53f506f75f74
bricebou <brice@reclic.dev>
2022-09-22 11:20:39 +0200

    On granularise la configuration de Bootstrap, aussi bien pour ce qui est des styles que des scripts ; on a la possibilité désormais de ne compiler que ce dont on a besoin.

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,16 @@
 // Import our custom CSS
 import '../scss/styles.scss';
+
+// Bootstrap Scripts
+import { Alert } from "bootstrap";
+import { Button } from "bootstrap";
+import { Carousel } from "bootstrap";
+import { Collapse } from "bootstrap";
+import { Dropdown } from "bootstrap";
+import { Modal } from "bootstrap";
+import { Offcanvas } from "bootstrap";
+import { Popover } from "bootstrap";
+import { ScrollSpy } from "bootstrap";
+import { Tab } from "bootstrap";
+import { Toast } from "bootstrap";
+import { Tooltip } from "bootstrap";
