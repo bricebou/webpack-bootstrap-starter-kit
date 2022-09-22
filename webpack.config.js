738eda88bb33a7161f040f21564ba4885ccc5015
bricebou <brice@reclic.dev>
2022-09-22 14:16:07 +0200

    On modifie la façon dont webpack-dev-server fonctionne : on n'écrit plus les fichiers sur le disque et on configure le chemin d'accès aux assets

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,7 @@ module.exports = {
     port: 8080,
     hot: true,
     devMiddleware: {
-      serverSideRender: true,
-      writeToDisk: true,
+      publicPath: '/assets',
     },
   },
   module: {
